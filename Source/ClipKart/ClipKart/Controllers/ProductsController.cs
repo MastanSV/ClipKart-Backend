1ba1f2bed8b863956000a80cd6f897526b927be7
Vali S, Mastan <mastanvali157@gmail.com>
2025-01-09 23:25:24 +0530

    Added basic products controller to get the hard coded list of products

---
+++ Source/ClipKart/ClipKart/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using ClipKart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClipKart.Controllers
+{
+    [ApiController]
+    [Route("Products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Index()
+        {
+            var products = new List<Product> { new Product() { Name = "Dairy", Description = "Write you fascinating things daily", Category = "Stationary", Price = 85 },
+            new Product{ Name="iPhone 12 mini", Description="Get closer and closer with your loved ones", Category="Tech/Electronics",Price=39000} };
+            return products;
+        }
+    }
+}
